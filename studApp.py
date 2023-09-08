17a86146ee3ae4c7b73aac58db9e43580866b749
EstherWT <71790914+EstherWT@users.noreply.github.com>
2023-09-08 22:15:20 +0800

    Update studApp.py

--- studApp.py
+++ studApp.py
@@ -105,6 +105,38 @@ def GetStud():
     finally:
         cursor.close()
 
+
+@app.route("/updatestud", methods=['POST'])
+def UpStud():
+    try:
+        stud = request.form['update_id']
+        name = request.form['update_name']
+
+        # Corrected SQL statement with placeholder
+        statement_get = "SELECT stud_name FROM student WHERE stud_id = %s"
+        cursor = db_conn.cursor()
+        cursor.execute(statement_get, (stud,))
+        
+        # Fetch the result
+        result = cursor.fetchone()
+
+        if result:
+
+            stud_name = result
+            statement = "UPDATE student SET stud_name = %s WHERE stud_id = %s"
+            cursor.execute(statement, (name, stud))
+           
+            return render_template('updateStudent.html', new=name, id=stud, old=stud_name)
+        else:
+            return render_template('updateError.html', id=stud)
+        
+    except Exception as e:
+        return str(e)
+        
+    finally:
+        cursor.close()
+
+
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
